877dcd5ff54711ba2ce70943751296b49bc1abbb
Junyang1996 <lawrencewanjunyang@gmail.com>
2024-01-10 14:50:12 -0500

    D17 the Quiz game

--- Day 17 Quiz game/main.py
+++ Day 17 Quiz game/main.py
@@ -5,9 +5,15 @@ question_bank = []
 
 # TODO 1
 # question bank, we will use it later as reference
-for item in question_data:
-    to_add = Question(item["text"], item["answer"])
+for item in question_data["results"]:
+    to_add = Question(text=item["question"], answer=item["correct_answer"])
     question_bank.append(to_add)
 
 Quiz1 = QuizBrain(question_bank)
-Quiz1.next_question()
+
+while Quiz1.still_have_questions():
+    Quiz1.next_question()
+
+print("\n\n")
+print("You've completed this quiz")
+print(f"Your final score was: {Quiz1.score}/{Quiz1.question_number}")
--- Day 17 Quiz game/quiz_brain.py
+++ Day 17 Quiz game/quiz_brain.py
@@ -2,8 +2,25 @@ class QuizBrain:
     def __init__(self, question_list):
         self.question_number = 0
         self.list = question_list
+        self.score = 0
 
     def next_question(self):
+        current_question = self.list[self.question_number]
+        current_question_question = current_question.text
+        self.question_number += 1
         """retrieve the current question from the current question number and ask the question to the user"""
-        answer = input(f"Q. {self.question_number+1}: {self.list[self.question_number].text} (True/False)?: ")
+        answer = input(f"Q. {self.question_number}: {current_question_question} (True/False)?: ")
+        self.check_answer(answer , current_question.answer)
 
+    def still_have_questions(self):
+        return self.question_number != len(self.list)
+
+    def check_answer(self, user_answer, correct_answer):
+        """if the user input is the same as the actual answer"""
+        if user_answer.lower() == correct_answer.lower():
+            print("You got it right!")
+            self.score += 1
+        else:
+            print("That's wrong.")
+        print(f"The correct answer was: {correct_answer}.")
+        print(f"Your current score is: {self.score}/{self.question_number}")
